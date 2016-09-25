508cf899fbc72c63cc02ed52510f82b81c74872e
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-09-25 08:40:37 -0400

    Upgrade eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,8 @@ module.exports = {
   extends: [
     "lddubeau-base/es5"
   ],
+  rules: {
+    "import/no-extraneous-dependencies":
+    ["error", {devDependencies: ["**/karma.conf.js"]}],
+  }
 };
